18b5df62a4eb27e3d10c86aee2ad99a1c0e1eff4
littlewin <littlewin.wang@sky-cloud.net>
2017-08-02 19:26:34 +0800

    update create category

---
+++ controllers/category.js
@@ -0,0 +1,42 @@
+/**
+ * @file 分类控制
+ * @author littlewin(littlewin.wang@gmail.com)
+ */
+
+const CategoryModel = require('models/category.model')
+
+class Category {
+  static async create (ctx) {
+    const category = ctx.request.body
+
+    // name validate
+    if (!category.name) {
+      ctx.throw(401, 'category name expected.')
+      return
+    }
+
+    if (await CategoryModel.findOne({ name: category.name }).exec()) {
+      ctx.status = 401
+      ctx.body = {
+        success: false,
+        message: "category already exist."
+      }
+      return
+    }
+
+    await new CategoryModel(category).save()
+      .then(() => {
+        ctx.status = 200
+        ctx.body = {
+          success: true,
+          message: "create category success."
+          // TODO sitemap && SEO
+        }
+      })
+      .catch(() => {
+        ctx.throw(401, 'create category error.')
+      })
+  }
+}
+
+module.exports = Category
